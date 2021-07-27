8de1394266d23491f5e9fbaa81e3f50a069ee422
Ivan Towlson <github@hestia.cc>
2021-07-27 14:28:02 +1200

    Broader conversational skills

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 fn main() {
   println!("Content-Type: text/html\n");
   println!("<p>Greetings, fellow human! Would you like to ingest some nutrients alongside me?  AS WE HUMANS DO</p>");
+  println!("<p>My dihydrogen monoxide level has fallen to 77.93 precent so I shall also ingest some liquid</p>");
 }
