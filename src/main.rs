c47dbb18560ab8f89c17e784568e54f7852768e2
Ivan Towlson <github@hestia.cc>
2021-07-27 14:17:12 +1200

    Made it more convincing

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
 fn main() {
   println!("Content-Type: text/html\n");
-  println!("<p>Greetings, fellow human! Would you like to ingest some nutrients alongside me?</p>");
+  println!("<p>Greetings, fellow human! Would you like to ingest some nutrients alongside me?  AS WE HUMANS DO</p>");
 }
